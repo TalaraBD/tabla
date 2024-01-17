edd10859005a2b628ce1e820f591f2f2385fdace
Davidct <97263791+davidct95@users.noreply.github.com>
2024-01-17 02:41:35 -0500

    Agregando componente 2

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Función para agregar filas a la tabla
-    function agregarFilas() {
+    function agregarFilas(datos, rutaBase) {
         var tbody = document.getElementById("componentesTable").getElementsByTagName("tbody")[0];
-
-        componentesData.features.forEach(function (fila) {
+    
+        datos.features.forEach(function (fila) {
             var tr = document.createElement("tr");
-
+    
             // Agregar celdas a la fila
-            tr.innerHTML += "<td><a href='componentes/componente13/componente13.html'>" + fila.properties.COMPONENTE + "</a></td>";
+            var componenteLink = rutaBase;
+            tr.innerHTML += "<td><a href='" + componenteLink + "'>" + fila.properties.COMPONENTE + "</a></td>";
             tr.innerHTML += "<td>" + fila.properties.NOMBRE + "</td>";
             tr.innerHTML += "<td>" + fila.properties.SECTOR + "</td>";
             tr.innerHTML += "<td>" + fila.properties.PAQUETE + "</td>";
@@ -15,12 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
             tr.innerHTML += "<td><button type='button' class='btn btn-primary' onclick='window.open(\"" + fila.properties.ENLACE_BIM + "\", \"_blank\")'>Abrir enlace</button></td>";
             tr.innerHTML += "<td></td>"; // Columna para actualizar (upd)
             tr.innerHTML += "<td></td>"; // Columna para eliminar (del)
-
+    
             // Agregar fila a tbody
             tbody.appendChild(tr);
         });
     }
 
+    var url2 = "componentes/componente2/componente2.html"
+    var url3 = "componentes/componente13/componente13.html"
+    var url4 = "componentes/componente4/componente4.html"
+
     // Llamar a la función para agregar filas
-    agregarFilas();
+    agregarFilas(componentesData2, url2);
+    agregarFilas(componentesData3, url3);
+    agregarFilas(componentesData4, url4);
 });
